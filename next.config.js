a1ebb955a34b0328590de7691dbe4a4920343df7
Leonardo Baron <leonardo.baron@eoschools.org>
2023-08-21 20:17:07 +0200

    test: test fix for assets

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   basePath: '/data',
   assetPrefix: '/data',
-  async rewrites() {
-    return [{ source: '/', destination: '/data', basePath: false }];
-  },
   async redirects() {
     return [{ source: '/', destination: '/data', basePath: false }];
   },
