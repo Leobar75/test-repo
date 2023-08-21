5d0e8d02e97de32b964bfe899eec18ecd80b5e61
Leonardo Baron <leonardo.baron@eoschools.org>
2023-08-21 21:21:35 +0200

    fix: removed basePath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/data',
-  assetPrefix: '/data',
-  async redirects() {
-    return [{ source: '/', destination: '/data', basePath: false, permanent: true }];
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
