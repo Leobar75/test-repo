83f9ed19fcaee12d8ff6baeb2ad4f6a5be55b68e
Leonardo Baron <leonardo.baron@eoschools.org>
2023-08-22 16:00:32 +0200

    test: only assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: '/data',
-  async rewrites() {
-    return [{ source: '/data/:path*', destination: '/:path*' }];
-  },
+  assetPrefix: '/data'
 };
 
 module.exports = nextConfig;
