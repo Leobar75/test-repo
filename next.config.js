98e90b20b19b9d35687bd19dbbbc9029aac5e872
Leonardo Baron <leonardo.baron@eoschools.org>
2023-08-22 18:22:56 +0200

    test: testvercel deployment

--- next.config.js
+++ next.config.js
@@ -1,30 +1,7 @@
-BASE_PREFIX_APP = '/data'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: BASE_PREFIX_APP,
-  async rewrites() {
-    return [
-      /** ASSET PREFIX */
-      {
-        source: `${BASE_PREFIX_APP}/_next/:path*`,
-        destination: '/_next/:path*',
-      },
-      /** IMAGE PREFIX */
-      {
-        source: `${BASE_PREFIX_APP}/images/:query*`,
-        destination: '/_next/image/:query*',
-      },
-      /** API PREFIX */
-      {
-        source: `${BASE_PREFIX_APP}/api/:path*`,
-        destination: '/api/:path*',
-      },
-    ];
-  },
+  basePath: '/data',
 };
-
-console.log(process.env.ASSETS_CDN);
 
 module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import Image from "next/image";
-import "@eoschools/portal2-common-components/index.css";
-import { Button, Label } from "@eoschools/portal2-common-components";
 
 export default function Home() {
   return (
@@ -42,9 +40,6 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Label>Test</Label>
-        <Button>Login</Button>
-
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
