cee8b2c57b737b53b5a2e82fe276e744d7cc168e
Leonardo Baron <leonardo.baron@eoschools.org>
2023-08-22 16:29:18 +0200

    test: added rewrites

--- next.config.js
+++ next.config.js
@@ -1,9 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.ASSETS_CDN ?? undefined,
+  assetPrefix: process.env.ASSETS_CDN,
+  async rewrites() {
+    return [
+      /** ASSET PREFIX */
+      {
+        source: `${process.env.ASSETS_CDN}/_next/:path*`,
+        destination: '_next/:path*',
+      },
+      /** IMAGE PREFIX */
+      {
+        source: `${process.env.ASSETS_CDN}/images/:query*`,
+        destination: '/_next/image/:query*',
+      },
+      /** API PREFIX */
+      {
+        source: `${process.env.ASSETS_CDN}/api/:path*`,
+        destination: '/api/:path*',
+      },
+    ];
+  },
 };
 
-console.log(process.env.ASSETS_CDN)
+console.log(process.env.ASSETS_CDN);
 
 module.exports = nextConfig;
